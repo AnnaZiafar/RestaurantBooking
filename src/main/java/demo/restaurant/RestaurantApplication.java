68b49d3b3dd569f4162ce8818788fce5f35a15e6
Anna Ziafar <annaaziafar@gmail.com>
2026-01-25 11:13:26 +0100

    Initial push with configurations to start project. Includes:
    -Docker compose
    -Dependencies in pom
    -Application properties

--- src/main/java/demo/restaurant/RestaurantApplication.java
+++ src/main/java/demo/restaurant/RestaurantApplication.java
@@ -1,13 +1,20 @@
 package demo.restaurant;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestaurantApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestaurantApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        Dotenv env = Dotenv.configure().load();
+        env.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
+        SpringApplication.run(RestaurantApplication.class, args);
+    }
 
 }
