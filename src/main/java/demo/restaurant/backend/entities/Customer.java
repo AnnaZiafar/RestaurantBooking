22b6362fa1a11d22c5c1d957f9f0d42a2e3f568c
Anna Ziafar <annaaziafar@gmail.com>
2026-01-25 19:23:14 +0100

    Creates three entites:
    -DiningTable
    -Customer
    -Booking
    
    Also updated Restaurant and OpeningHours entity so they all have correct relationships.

---
+++ src/main/java/demo/restaurant/backend/entities/Customer.java
@@ -0,0 +1,35 @@
+package demo.restaurant.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+    private String lastname;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
+    @Column(unique = true)
+    private String email;
+
+    public Customer(String firstname, String lastname, String phoneNumber, String email){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
