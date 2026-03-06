86a9d514ca5d0bcfe1941eced2f63419f7c9ada9
Anna Ziafar <annaaziafar@gmail.com>
2026-03-06 12:54:31 +0100

    Create restaurant-grid with styled restaurant cards

--- src/main/java/demo/restaurant/controllers/RestaurantController.java
+++ src/main/java/demo/restaurant/controllers/RestaurantController.java
@@ -2,7 +2,6 @@ package demo.restaurant.controllers;
 
 import demo.restaurant.backend.services.RestaurantService;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,11 +10,16 @@ public class RestaurantController {
 
     RestaurantService restaurantService;
 
-    private RestaurantController(RestaurantService restaurantService){
+    public RestaurantController(RestaurantService restaurantService){
         this.restaurantService = restaurantService;
     }
 
-    @GetMapping("/main")
+    @GetMapping("/get-header")
+    public String getHeader(){
+        return "header";
+    }
+
+    @GetMapping("/")
     public String loadRestaurants(Model model){
         model.addAttribute("restaurants", restaurantService.getAll());
         return "main";
