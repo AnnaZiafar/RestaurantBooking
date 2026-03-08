a1927175382d07f734361e954a8d4afb97b7e82a
Anna Ziafar <annaaziafar@gmail.com>
2026-03-08 11:49:08 +0100

    Search-bar performs search (Except for wen all cities are chosen)

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,7 @@
+const select = document.querySelector('.city-selection');
+const options = document.querySelector('.city-options');
+let selectedCity = null;
+
 /**
  * Handles city selection and filtering of restaurant cards.
  * Listens for clicks to toggle the visibility of the dropdown menu and updates
@@ -5,22 +9,20 @@
  * @listens document:click
  */
 document.addEventListener('click', (e) => {
-    const select = document.querySelector('.city-selection');
-    const options = document.querySelector('.city-options');
     const cards = document.querySelectorAll('.restaurant-cards');
 
     if(e.target.closest('.city-selection')) options.classList.toggle('show');
     else(options.classList.remove('show'));
 
     if(e.target.closest('.city-options div')){
-        const selectedCity = e.target.textContent.trim();
+        selectedCity = e.target.textContent.trim();
         select.textContent = selectedCity;
         options.classList.remove('show')
 
         cards.forEach(card => {
             const city = card.dataset.city.trim();
 
-            if (selectedCity === "Välj en stad" || selectedCity.toLowerCase() === city.toLowerCase()) {
+            if (selectedCity === "Alla städer" || selectedCity.toLowerCase() === city.toLowerCase()) {
                 card.style.display = "block";
             } else {
                 card.style.display = "none";
@@ -28,3 +30,32 @@ document.addEventListener('click', (e) => {
         })
     }
 })
+
+/**
+ * Handles searching/filtering of restaurants based on name or category.
+ * Listens for both button clicks and Enter key presses.
+ */
+function setupSearch(){
+    const input = document.getElementById('restaurant-search-input');
+    const searchContainer = document.getElementById('search-container');
+    const cards = document.querySelectorAll('.restaurant-cards');
+
+    const performSearch = (e) => {
+        if (e.type === 'submit') e.preventDefault()
+        const query = input.value.trim().toLowerCase();
+
+        cards.forEach(card => {
+            const restaurantName = card.querySelector('.restaurant-card-info span').textContent.trim().toLowerCase()
+            const city = card.dataset.city.trim();
+
+            if(restaurantName.includes(query) && selectedCity === city || selectedCity === 'Alla städer')
+                card.style.display = 'block';
+            else
+                card.style.display = 'none';
+        })
+    }
+
+    if(searchContainer) searchContainer.addEventListener('submit', performSearch)
+}
+
+document.addEventListener('DOMContentLoaded', setupSearch);
